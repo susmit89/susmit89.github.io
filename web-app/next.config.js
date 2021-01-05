7f79fdc19e75d57c1f6fa6eff78c6550d1165452
susmit89 <susmit89@gmail.com>
2021-01-04 17:46:57 -0800

    ef

--- web-app/next.config.js
+++ web-app/next.config.js
@@ -2,5 +2,5 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://raw.githubusercontent.com/susmit89/susmit89.github.io/gh-pages/' : '',
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/susmit89/susmit89.github.io/gh-pages' : '',
 }
